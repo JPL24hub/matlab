6c047569d869654a3d2a287c605aa1fecb528391
Poncha <joelponcha24@gmail.com>
2023-02-15 18:33:39 -0500

    update

---
+++ Complete projective point addition/binary_inversion_algorithm.m
@@ -0,0 +1,48 @@
+function [R] = binary_inversion_algorithm(a, p)
+
+    u = a;
+    v = p;
+    x = 1;
+    y = 0;
+
+    while u ~= 0
+        while mod(vpi(u), vpi(2)) == 0
+            u = vpi(u) / vpi(2);
+            if mod(vpi(x), vpi(2)) == 0
+                x = vpi(x) / vpi(2);
+            else
+                x = (vpi(x) + vpi(p)) / vpi(2);
+            end
+        end
+        while mod(vpi(v), vpi(2)) == 0
+            v = vpi(v) / vpi(2);
+            if mod(vpi(y), vpi(2)) == 0
+                y = vpi(y) / vpi(2);
+            else
+                y = (vpi(y) + vpi(p)) / vpi(2);
+            end
+        end
+        if vpi(u) >= vpi(v)
+            u = vpi(u) - vpi(v);
+            if vpi(x) > vpi(y)
+                x = vpi(x) - vpi(y);
+            else
+                x = vpi(x) + vpi(p) - vpi(y);
+            end
+        else
+            v = vpi(v) - vpi(u);
+            if vpi(y) > vpi(x)
+                y = vpi(y) - vpi(x);
+            else
+                y = vpi(y) + vpi(p) - vpi(x);
+            end
+        end
+    end
+    
+    if u == 1
+        R = mod(vpi(x), vpi(p));
+    elseif v == 1
+        R = mod(vpi(y), vpi(p));
+    end
+
+end
--- Complete projective point addition/invers.m
+++
@@ -1,6 +0,0 @@
-function[X, Y, Z] = invers(X, Y, Z, primeP)
-Z_invers = Z^(primeP+2);
-X = mod(vpi(X)*vpi(Z_invers), vpi(primeP));
-Y = mod(vpi(Y)*vpi(Z_invers), vpi(primeP));
-Z = mod(vpi(Z)*vpi(Z_invers), vpi(primeP));
-end
