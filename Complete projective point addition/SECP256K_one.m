e6b93ed981e22d026e5264502b473722bbdeb0d8
Poncha <joelponcha24@gmail.com>
2023-03-19 15:46:06 -0400

    UPDATE

--- Complete projective point addition/SECP256K_one.m
+++ Complete projective point addition/SECP256K_one.m
@@ -4,7 +4,7 @@ clc
 
 %% Constants for SECP256K1
 
-primeP = 119;
+primeP = 10001;
 XP = 1;
 YP = 2;
 b=3;
@@ -14,10 +14,11 @@ m='2';
 % primeP = '115792089237316195423570985008687907853269984665640564039457584007908834671663';
 % XP = '55066263022277343669578718895168534326250603453777594175500187360389116729240';
 % YP = '32670510020758816978083085130507043184471273380659243275938904335757337482424';
-% b=3;
+% b=7;
 
 ZP = 1;
-m_binary = hexToBinaryVector(m, [],'LSBFirst');
+%m_binary = hexToBinaryVector(m, [],'LSBFirst');
+m_binary = flip(dec2bin(hex2dec(m)), 2);
 i = length(m_binary)-1;
 XR = XP;
 YR = YP;
@@ -28,7 +29,7 @@ b3 = b*3;
 tic; % start time
 while i>0 
     [XR, YR, ZR] = projective_point_addition(XR,YR,ZR, XR,YR,ZR, b3, primeP);
-    if m_binary(i)==1
+    if m_binary(i)=='1'
         [XR, YR, ZR] = projective_point_addition(XR,YR,ZR, XP,YP,ZP, b3, primeP);
     end   
     i=i-1;
